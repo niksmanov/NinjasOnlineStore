1e082b37c6b898d280826b7316885e4c34ec5ab7
vasilvalkov <vasil.valkov@gmail.com>
2017-06-07 00:46:07 +0300

    Introduced ServiceLocator to unbind Core from the App

---
+++ NinjasOnlineStore.App/ServiceLocator.cs
@@ -0,0 +1,22 @@
+﻿using NinjasOnlineStore.App.Core.Commands.Contracts;
+using NinjasOnlineStore.Core.Contracts;
+using Ninject;
+using System;
+
+namespace NinjasOnlineStore.App
+{
+    public class ServiceLocator : IServiceLocator
+    {
+        private readonly IKernel kernel;
+
+        public ServiceLocator(IKernel kernel)
+        {
+            this.kernel = kernel;
+        }
+
+        public ICommand GetCommand(Type commandType)
+        {
+            return (ICommand)this.kernel.Get(commandType);
+        }
+    }
+}
