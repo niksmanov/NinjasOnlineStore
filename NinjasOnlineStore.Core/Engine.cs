b9ced68582bed2af24d15b9c0c2a7680b4f323b0
vasilvalkov <vasil.valkov@gmail.com>
2017-06-07 00:48:34 +0300

    Implemented Engine to listen to and process commands

---
+++ NinjasOnlineStore.Core/Engine.cs
@@ -0,0 +1,81 @@
+﻿using NinjasOnlineStore.App.Core.Contracts;
+using NinjasOnlineStore.Core.Contracts;
+using System;
+
+namespace NinjasOnlineStore.App.Core
+{
+    public class Engine : IEngine
+    {
+        private const string TerminationCommand = "Exit";
+        private const string NullProvidersExceptionMessage = "cannot be null.";
+
+        private readonly IReader reader;
+        private readonly IWriter writer;
+        private readonly ICommandParser parser;
+        private readonly ICommandFactory commandFactory;
+        
+        public Engine(IReader readerProvider, IWriter writerProvider, ICommandParser parserProvider, ICommandFactory commandFactory)
+        {
+            if (readerProvider == null)
+            {
+                throw new ArgumentNullException($"Reader {NullProvidersExceptionMessage}");
+            }
+
+            if (writerProvider == null)
+            {
+                throw new ArgumentNullException($"Writer {NullProvidersExceptionMessage}");
+            }
+
+            if (parserProvider == null)
+            {
+                throw new ArgumentNullException($"Parser {NullProvidersExceptionMessage}");
+            }
+
+            if (commandFactory == null)
+            {
+                throw new ArgumentNullException($"Parser {NullProvidersExceptionMessage}");
+            }
+
+            this.reader = readerProvider;
+            this.writer = writerProvider;
+            this.parser = parserProvider;
+            this.commandFactory = commandFactory;
+        }
+
+        public void Start()
+        {
+            while (true)
+            {
+                try
+                {
+                    var commandAsString = this.reader.ReadLine();
+
+                    if (commandAsString == TerminationCommand)
+                    {
+                        break;
+                    }
+
+                    this.ProcessCommand(commandAsString);
+                }
+                catch (Exception ex)
+                {
+                    this.writer.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        private void ProcessCommand(string commandAsString)
+        {
+            if (string.IsNullOrWhiteSpace(commandAsString))
+            {
+                throw new ArgumentNullException("Command cannot be null or empty.");
+            }
+
+            var command = this.commandFactory.GetCommand(commandAsString);
+            var parameters = this.parser.ParseParameters(commandAsString);
+
+            var executionResult = command.Execute(parameters);
+            this.writer.WriteLine(executionResult);
+        }
+    }
+}
