a0e12bc1ee9b36771bd6172ce6d3ec9c6268895b
vasilvalkov <vasil.valkov@gmail.com>
2017-06-07 00:44:31 +0300

    Instantiate the container and start the engine. Removed callings to database importers

--- NinjasOnlineStore.App/StartUp.cs
+++ NinjasOnlineStore.App/StartUp.cs
@@ -1,6 +1,8 @@
-﻿using NinjasOnlineStore.JSON;
+﻿using NinjasOnlineStore.Core.Contracts;
+using NinjasOnlineStore.JSON;
 using NinjasOnlineStore.PostgreSQL;
 using NinjasOnlineStore.SqLite;
+using Ninject;
 
 namespace NinjasOnlineStore.App
 {
@@ -8,12 +10,21 @@ namespace NinjasOnlineStore.App
     {
         static void Main()
         {
-            string jsonFilePath = "../../../NinjasOnlineStore.JSON/DATA.json";
-            JsonImporter.Import(jsonFilePath);
+            IKernel kernel = new StandardKernel(new NinjasOnlineStoreModule());
 
-            SqLiteImporter.Import();
+            var engine = kernel.Get<IEngine>();
+            engine.Start();
 
-            PostgreSQLImporter.Import();
+            // To run the commented lines below
+            // start the app and enter command: InitializeDatabase
+            // ---------------------------------------------------
+
+            //string jsonFilePath = "../../../NinjasOnlineStore.JSON/DATA.json";
+            //JsonImporter.Import(jsonFilePath);
+
+            //SqLiteImporter.Import();
+
+            //PostgreSQLImporter.Import();
         }
     }
 }
