01aa3dc0325ef185f5a54fe911cb88b9b34b8f3e
vasilvalkov <vasil.valkov@gmail.com>
2017-06-07 00:52:32 +0300

    Introduced command to import/initialize available databases

---
+++ NinjasOnlineStore.Core/Commands/InitializeDatabaseCommand.cs
@@ -0,0 +1,34 @@
+﻿using NinjasOnlineStore.App.Core.Commands.Contracts;
+using NinjasOnlineStore.JSON;
+using NinjasOnlineStore.PostgreSQL;
+using NinjasOnlineStore.SqLite;
+using System;
+using System.Collections.Generic;
+
+namespace NinjasOnlineStore.Core.Commands
+{
+    public class InitializeDatabaseCommand : ICommand
+    {
+        public string Execute(IList<string> parameters)
+        {
+            // TODO: Extract jsonFilePath as parameter
+            string jsonFilePath = "../../../NinjasOnlineStore.JSON/DATA.json";
+
+            // TODO: Inject IWriter to replace Console
+            Console.WriteLine("Initializing JSON...");
+
+            // TODO: Inject reporter and make Import method non static
+            JsonImporter.Import(jsonFilePath);
+
+            Console.WriteLine("Initializing SqLite...");
+
+            // TODO: Split this command into three separate commands for each importer
+            SqLiteImporter.Import();
+
+            Console.WriteLine("Initializing Postgres...");
+            PostgreSQLImporter.Import();
+
+            return "Initialization succeeded!";
+        }
+    }
+}
