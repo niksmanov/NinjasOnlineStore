7c4e6992c5f004f59911f9417b769921bee85600
vasilvalkov <vasil.valkov@gmail.com>
2017-06-07 00:45:06 +0300

    Setup container bindings

---
+++ NinjasOnlineStore.App/NinjasOnlineStoreModule.cs
@@ -0,0 +1,23 @@
+﻿using NinjasOnlineStore.App.Core;
+using NinjasOnlineStore.App.Core.Contracts;
+using NinjasOnlineStore.App.Core.Providers;
+using NinjasOnlineStore.Core.Contracts;
+using Ninject.Modules;
+
+namespace NinjasOnlineStore.App
+{
+    public class NinjasOnlineStoreModule : NinjectModule
+    {
+        public override void Load()
+        {
+            this.Bind<IReader>().To<ConsoleReader>().InSingletonScope();
+            this.Bind<IWriter>().To<ConsoleWriter>().InSingletonScope();
+            this.Bind<ICommandParser>().To<CommandParser>();
+            
+            this.Bind<ICommandFactory>().To<CommandsFactory>().InSingletonScope();
+
+            this.Bind<IEngine>().To<Engine>().InSingletonScope();
+            this.Bind<IServiceLocator>().To<ServiceLocator>();
+        }
+    }
+}
