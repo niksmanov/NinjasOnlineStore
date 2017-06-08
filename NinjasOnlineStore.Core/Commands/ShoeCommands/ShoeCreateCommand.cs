c22b6187863f1ca1aeaf7104345e92fedaf4f5ca
Hristo <hrvasilev87@gmail.com>
2017-06-08 19:42:57 +0300

    Added ShoeCreateCommand.cs and ShoeDeleteCommand.cs

---
+++ NinjasOnlineStore.Core/Commands/ShoeCommands/ShoeCreateCommand.cs
@@ -0,0 +1,38 @@
+﻿using NinjasOnlineStore.App.Core.Commands.Contracts;
+using NinjasOnlineStore.JSON;
+using NinjasOnlineStore.SqlServer.Models;
+using System;
+using System.Collections.Generic;
+
+namespace NinjasOnlineStore.Core.Commands.JacketCommands
+{
+    public class ShoeCreateCommand : ICommand
+    {
+        public string Execute(IList<string> parameters)
+        {
+            var database = JsonImporter.SQLServerDbConnecton();
+
+            var brand = parameters[0];
+            var model = parameters[1];
+            var color = parameters[2];
+            var type = parameters[3];
+            var size = parameters[4];
+            var price = parameters[5];
+
+            var shoe = new Shoe();
+
+            shoe.BrandId = 1;
+            shoe.ModelId = 2;
+            shoe.ColorId = 3;
+            shoe.KindId = 4;
+            shoe.SizeId = 5;
+            shoe.Price = Decimal.Parse(price);
+
+            database.Shoes.Add(shoe);
+
+            database.SaveChanges();
+
+            return "Create command executed successfully";
+        }
+    }
+}
---
+++ NinjasOnlineStore.Core/Commands/ShoeCommands/ShoeDeleteCommand.cs
@@ -0,0 +1,44 @@
+﻿using NinjasOnlineStore.App.Core.Commands.Contracts;
+using NinjasOnlineStore.App.Core.Contracts;
+using NinjasOnlineStore.JSON;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NinjasOnlineStore.Core.Commands.ShoeCommands
+{
+    public class ShoeDeleteCommand : ICommand
+    {
+        private readonly IWriter writer;
+        private readonly IReader reader;
+
+        public ShoeDeleteCommand(IWriter writer, IReader reader)
+        {
+            this.writer = writer;
+            this.reader = reader;
+        }
+
+        public string Execute(IList<string> parameters)
+        {
+            var database = JsonImporter.SQLServerDbConnecton();
+            var shoesCollection = database.Shoes.ToList();
+
+            foreach (var shoe in shoesCollection)
+            {
+                this.writer.WriteLine($"Id: {shoe.Id}, Brand: {shoe.Brand.Name}, Model: {shoe.Model.Name}, Color: {shoe.Color.Name}, Type: {shoe.Kind.Name}, Size: {shoe.Size.Name}, Price: {shoe.Price} EUR");
+            }
+
+            this.writer.Write("Please provide ID of the item you want to remove: ");
+
+            var itemId = int.Parse(this.reader.ReadLine());
+            var shoesToDelete = shoesCollection.First(j => j.Id == itemId);
+
+            database.Shoes.Remove(shoesToDelete);
+
+            this.writer.WriteLine($"Shoes with ID: {itemId} was successfuly removed!");
+
+            database.SaveChanges();
+
+            return "Command executed successfully";
+        }
+    }
+}
