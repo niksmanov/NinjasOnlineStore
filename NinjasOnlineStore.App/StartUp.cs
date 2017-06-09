983d5e50806f61e1f6d6deb8a6427272789e50bf
Hristo <hrvasilev87@gmail.com>
2017-06-10 01:03:14 +0300

    Listing available commands.

--- NinjasOnlineStore.App/StartUp.cs
+++ NinjasOnlineStore.App/StartUp.cs
@@ -1,5 +1,8 @@
-﻿using NinjasOnlineStore.Core.Contracts;
+﻿using NinjasOnlineStore.App.Core.Commands.Contracts;
+using NinjasOnlineStore.Core.Contracts;
 using Ninject;
+using System;
+using System.Linq;
 
 namespace NinjasOnlineStore.App
 {
@@ -10,8 +13,20 @@ namespace NinjasOnlineStore.App
             IKernel kernel = new StandardKernel(new NinjasOnlineStoreModule());
 
             var engine = kernel.Get<IEngine>();
-            engine.Start();
 
+            var type = typeof(ICommand);
+            var commands = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(s => s.GetTypes())
+                .Where(p => type.IsAssignableFrom(p) && !p.IsInterface)
+                .Select(t => t.Name)
+                .Select(c => c.Split(new string[] { "Command" }, StringSplitOptions.RemoveEmptyEntries)[0]).ToList();
+            commands.Sort();
+
+            Console.WriteLine("Commands list!");
+            Console.WriteLine("Please use InitializeDatabase first!", Environment.NewLine);
+            Console.WriteLine(string.Join(Environment.NewLine, commands));
+
+            engine.Start();
             //To create the database enter the command: InitializeDatabase
 
             //To create pdf report from SqlServer, PostgreSql and SqLite enter the command: GenerateReport
